03109eac4c455fca9613ac5baa6dba1eda7e24ec
Elyoneondra <hannelore.lmtt@gmail.com>
2020-04-06 15:03:04 +0200

    getting connection with the API

--- tenor-api/src/App.js
+++ tenor-api/src/App.js
@@ -4,15 +4,32 @@ import axios from 'axios';
 export default class App extends Component {
      constructor(props) {
           super(props);
-          this.state = {};
+          this.state = {
+               gifs: {
+                    error: false,
+                    data: [],
+               },
+          };
      } // end of constructor
+
      summonTheGifs = (mood) => {
           axios.get(
-               'https://api.tenor.com/v1/registershare?id=8776030&key=WDPYDKUBMATL&q=excited'
-          ).then((results) => {
-               console.log(results.data);
-          });
-     };
+               'https://api.tenor.com/v1/search?key=WDPYDKUBMATL&q=excited'
+          )
+               .then((results) => {
+                    // console.log(results.data.results);
+                    this.setState({
+                         gifs: {
+                              ...this.state.gifs,
+                              data: [...results.data.results],
+                         },
+                    });
+               })
+               .catch((error) => {
+                    console.log(error);
+               });
+     }; // end of summonTheGifs
+
      render() {
           return <p onClick={this.summonTheGifs}>Hello</p>;
      }
